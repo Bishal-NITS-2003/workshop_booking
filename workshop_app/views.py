25b1bf097c57fc5248bdd37818748356b0a12196
Bishal Das <dasbishal1717@gmail.com>
2025-09-13 11:48:23 +0530

    Fixed a small bug redirecting user to admin if superuser

--- workshop_app/views.py
+++ workshop_app/views.py
@@ -39,6 +39,8 @@ __credits__ = ["Mahesh Gudi", "Aditya P.", "Ankit Javalkar",
 # Helper functions
 
 def is_email_checked(user):
+    if user.is_superuser:
+        return True
     return user.profile.is_email_verified
 
 
@@ -61,6 +63,8 @@ def index(request):
                       Redirect to respective landing page according to position"""
     user = request.user
     if user.is_authenticated and is_email_checked(user):
+        if user.is_superuser:
+            return redirect('/admin')
         return redirect(get_landing_page(user))
 
     return redirect(reverse('workshop_app:login'))
@@ -71,24 +75,27 @@ def index(request):
 # TODO: Forgot password workflow
 def user_login(request):
     """User Login"""
-    user = request.user
-    if user.is_superuser:
-        return redirect('/admin')
-    if user.is_authenticated:
-        return redirect(get_landing_page(user))
-
     if request.method == "POST":
         form = UserLoginForm(request.POST)
         if form.is_valid():
             user = form.cleaned_data
+            login(request, user)
+            # Now user is authenticated, check for superuser
+            if user.is_superuser:
+                return redirect('/admin')
             if user.profile.is_email_verified:
-                login(request, user)
                 return redirect(get_landing_page(user))
             else:
                 return render(request, 'workshop_app/activation.html')
         else:
             return render(request, 'workshop_app/login.html', {"form": form})
     else:
+        # For GET requests, if already logged in, redirect accordingly
+        user = request.user
+        if user.is_authenticated:
+            if user.is_superuser:
+                return redirect('/admin')
+            return redirect(get_landing_page(user))
         form = UserLoginForm()
         return render(request, 'workshop_app/login.html', {"form": form})
 
